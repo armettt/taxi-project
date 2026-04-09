9dfdb79e7e584b5295ff818cce48222207bb0467
armettt <matveys090512@gmail.com>
2026-04-09 17:13:57 +0300

    Update bot.py

---
+++ app/passenger/bot.py
@@ -0,0 +1,15 @@
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from app.core.config import BOT_TOKEN_PASSENGER
+from app.passenger.handlers import start, order, cancel
+
+
+def create_passenger_bot():
+    bot = Bot(token=BOT_TOKEN_PASSENGER)
+    dp = Dispatcher(storage=MemoryStorage())
+
+    dp.include_router(start.router)
+    dp.include_router(order.router)
+    dp.include_router(cancel.router)
+
+    return bot, dp

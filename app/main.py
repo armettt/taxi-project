6416183ca47e92a336a12238cdf61e2f862b82a3
armettt <matveys090512@gmail.com>
2026-04-09 20:08:26 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,23 +1,12 @@
 import asyncio
-from app.core.logger import logging
+import logging
 from app.core.database import create_pool, init_db
-from app.passenger.bot import create_passenger_bot
-from app.driver.bot import create_driver_bot
 
+logging.basicConfig(level=logging.INFO)
 
 async def main():
-    await create_pool()
     await init_db()
-
-    passenger_bot, passenger_dp = create_passenger_bot()
-    driver_bot, driver_dp = create_driver_bot()
-
-    await asyncio.gather(
-        passenger_dp.start_polling(passenger_bot),
-        driver_dp.start_polling(driver_bot)
-    )
-
+    logging.info("Боты запущены")
 
 if __name__ == "__main__":
-    logging.info("Боты запущены")
     asyncio.run(main())
